bf5a796f78f679df71353b246ab0d187fcc98bcb
MoussioG4 <73383266+MoussioG4@users.noreply.github.com>
2023-03-08 01:27:43 +0200

    20230308_UpdateI

--- js/script.js
+++ js/script.js
@@ -26,3 +26,48 @@ var x = setInterval(function() {
     document.getElementById("countdown").innerHTML = "The wedding was frickin great! :) ";
   }
 }, 1000);
+
+//Adjust the navbar backround from transparent (scrolled to top) to black (scrolled down)
+window.addEventListener('scroll', function navbar_background(){
+  var classes = document.querySelector("#navbar").className;
+  const scroll = window.scrollY;  
+  
+  if (scroll == 0) {
+    if (classes.indexOf("navbar-style-init") == -1){
+      classes = classes.replace(new RegExp("navbar-style-scroll", "g"), "navbar-style-init");
+      document.querySelector("#navbar").className = classes;
+    }
+  }
+  else {
+    if (classes.indexOf("navbar-style-scroll") == -1){
+      classes = classes.replace(new RegExp("navbar-style-init", "g"), "navbar-style-scroll");
+      document.querySelector("#navbar").className = classes;
+    }
+  };
+});
+
+// On page load (before images or css)
+document.addEventListener("DOMContentLoaded", function (event) {
+ 
+});
+
+(function (global) {
+
+  var fn = {};
+
+  // Convinienve function for inserting innerHtmL for 'select'
+  var insertHtml = function (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'
+var showLoading = function(selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='pictures/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+}
+
+global.fn = fn;
+
+})(window);
