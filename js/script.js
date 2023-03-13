4b70e9fb9dc0c96c93e306dc2b17f110fa91369e
MoussioG4 <73383266+MoussioG4@users.noreply.github.com>
2023-03-13 23:28:00 +0200

    20230313_Update

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
-//Adjust the navbar backround from transparent (scrolled to top) to black (scrolled down)
-document.addEventListener('scroll', function navbar_background(){
-  let classes = document.querySelector("#navbar").className;
-  const scroll = window.scrollY;  
-  
-  if (scroll < 30) {
-    if (classes.indexOf("navbar-style-init") == -1){
-      classes = classes.replace(new RegExp("navbar-style-scroll", "g"), "navbar-style-init");
-      document.querySelector("#navbar").className = classes;
+//Adjust the navbar backround from transparent (scrolled to top) to black (scrolled down) only on the home page
+document.addEventListener('scroll', window.onload = function navbar_background(){
+  var myEle = document.getElementById("countdown");
+  if(myEle != null) {
+    let classes = document.querySelector("#navbar").className;
+    const scroll = window.scrollY;
+      
+    if (scroll < 30) {
+      if (classes.indexOf("navbar-style-init") == -1){
+        classes = classes.replace(new RegExp("navbar-style-scroll", "g"), "navbar-style-init");
+        document.querySelector("#navbar").className = classes;
+      }
     }
-  }
-  else {
-    if (classes.indexOf("navbar-style-scroll") == -1){
-      classes = classes.replace(new RegExp("navbar-style-init", "g"), "navbar-style-scroll");
-      document.querySelector("#navbar").className = classes;
+    else {
+      if (classes.indexOf("navbar-style-scroll") == -1){
+        classes = classes.replace(new RegExp("navbar-style-init", "g"), "navbar-style-scroll");
+        document.querySelector("#navbar").className = classes;
+      }
     }
   };
-});
-
+})
+  
+// Function for the countdown, to fire after complete content was loaded so that the "countdown"-element can be found
+window.onload = function() {
   // Set the date we're counting down to
-var countDownDate = new Date("Sep 9, 2023 17:00:00").getTime();
+  var countDownDate = new Date("Sep 9, 2023 17:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+  // Run the Countdown only in the home view, when the "countdown"-element is present
+  var myEle = document.getElementById("countdown");
+  if(myEle != null) {
 
   // Get today's date and time
   var now = new Date().getTime();
@@ -44,15 +52,18 @@ var x = setInterval(function() {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "The wedding was frickin great! :) ";
   }
+}
 }, 1000);
 
 
+};
 
 (function (global) {
 
   var fn = {};
 
   var homeHtml = "snippets/home-snippet.html";
+  var rsvpHtml = "snippets/rsvp-snippet.html";
 
   // Convinienve function for inserting innerHtmL for 'select'
   var insertHtml = function (selector, html) {
@@ -63,7 +74,7 @@ var x = setInterval(function() {
   // Show loading icon inside element identified by 'selector'
   var showLoading = function(selector) {
       var html = "<div class='text-center'>";
-      html += "<img src='pictures/ajax-loader.gif'></div>";
+      html += "<img src='pics/ajax-loader.gif'></div>";
       insertHtml(selector, html);
     };
   
@@ -76,8 +87,22 @@ var x = setInterval(function() {
       document.querySelector("#main-content").innerHTML = responseText;
   },
   false);
+
+  // On click on RSVP button load RSVP page
+  fn.loadRSVP = function (){
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(rsvpHtml, function (responseText) {
+    document.querySelector("#main-content").innerHTML = responseText;
+    let classes = document.querySelector("#navbar").className;
+    if (classes.indexOf("navbar-style-scroll") == -1){
+        classes = classes.replace(new RegExp("navbar-style-init", "g"), "navbar-style-scroll");
+        document.querySelector("#navbar").className = classes;
+    };  
+    },
+    false);
+  };
 });
 
+  global.$fn = fn;
 
 })(window);
-
