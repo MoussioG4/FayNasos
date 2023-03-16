6818b894ebff86ec7bf120a5a93a3fe9575e5deb
MoussioG4 <73383266+MoussioG4@users.noreply.github.com>
2023-03-16 18:29:29 +0200

    20230316_UpdateIII

---
+++ php/send_mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$given_name = $_POST["given_name_input"];
+$surname = $_POST["surname_input"];
+$contact_selector = $_POST["contact_selector"];
+$phone = $_POST["phone_input"];
+$mail = $_POST["mail_input"];
+$attendance_selector = $_POST["attendance_selector"];
+$date_selector = $_POST["date_selector"];
+$date_name = $_POST["date_name"];
+$allergies_selector = $_POST["allergies_selector"];
+$message_input = $_POST["message_input"];
